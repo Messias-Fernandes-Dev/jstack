6987a663f73fe359bd6e3fdddf3516fcd8c685dd
Messias Fernandes <dev.messiasfer@gmail.com>
2025-08-02 20:25:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const {printName, lastName} = require('./printName')
+
+printName(`Messias ${lastName}`)
+
+// Modulos são conjustos de código
+
+// 3 tipos de modulos 
+ // -> TODOS OS ARQUIVOS JAVASCRIPT SÃO MÓDULOS;
+ // -> NATIVOS - HTTP - V8 - DNS
+ // -> npm (Node Package Manager);
+
+ 
+
---
+++ printName.js
@@ -0,0 +1,8 @@
+exports.printName = (name) => {
+  console.log(name)
+}
+exports.lastName = 'Fernandes';
+
+
+
+//module.exports = {printName, lastName};
