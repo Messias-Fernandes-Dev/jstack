8b9d873f6d8328c0dc29ba823a62f706bfbffdc5
Messias Fernandes <dev.messiasfer@gmail.com>
2025-08-07 17:43:00 -0300

    Query Params

--- firstAPI/src/index.js
+++ firstAPI/src/index.js
@@ -1,22 +1,30 @@
 const http = require("http");
+const {URL} = require("url");
 
 const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`)
+
+  
+
+  console.log(
+    `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+  );
 
   const route = routes.find(
     (routeObj) =>
-      routeObj.endpois === request.url && routeObj.method === request.method
+      routeObj.endpois === parsedUrl.pathname &&
+      routeObj.method === request.method
   );
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
-
 });
 
 server.listen(3000, () =>
