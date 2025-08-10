266f2a75b17748d38b289cb584563c7f3047abb6
Messias Fernandes <dev.messiasfer@gmail.com>
2025-08-10 11:53:29 -0300

    criando metodo response.send

--- firstAPI/src/index.js
+++ firstAPI/src/index.js
@@ -26,7 +26,12 @@ const server = http.createServer((request, response) => {
 
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams);
-    request.params = {id}
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(body));
+    };
 
     route.handler(request, response);
   } else {
